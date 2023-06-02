8b81c6a2c660657f48f5e74b380e230f5c45d661
jaeho <gumsanim1993@gmail.com>
2023-06-03 02:11:11 +0900

    저장된 url 없을 때 보여줄 alert ui 작업

--- src/hooks/useUrl.tsx
+++ src/hooks/useUrl.tsx
@@ -1,3 +1,97 @@
-const useUrl = () => {};
+import React, { useEffect, useState, useCallback } from "react";
+import Layout from "../layouts/Layout";
+import localStorage from "../utils/localStorage";
+import uuid from "react-uuid";
+import { localStorageKeys } from "../constants/dummy";
+import { IUrlItem } from "../@types/data.types";
+
+const useUrl = () => {
+  const [urlList, setUrlList] = useState<Array<IUrlItem>>([]);
+  const [urlSearchInput, setUrlSearchInput] = useState("");
+  const uniqueId = uuid();
+
+  const urlSearchInputHandler = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setUrlSearchInput(event.target.value);
+  };
+
+  const urlFromLocalStorage = localStorage.getData(
+    localStorageKeys.url_list
+  ) as string;
+
+  const addUrlHandler = () => {
+    /*
+      local storage에 url_list라는 이름으로 url 데이터가 존재한다면
+      기존 데이터에 새로운 url 정보를 삽입 후 url list state 업데이트
+    */
+    if (urlFromLocalStorage) {
+      const addedUrlList = JSON.parse(urlFromLocalStorage).concat({
+        id: uniqueId,
+        url: urlSearchInput,
+      });
+      localStorage.setData(
+        localStorageKeys.url_list,
+        JSON.stringify(addedUrlList)
+      );
+      setUrlSearchInput("");
+      return setUrlList(addedUrlList);
+    }
+    /*
+      local storage에 url_list라는 이름으로 url 데이터가 존재 하지 않는다면
+      url_list라는 이름으로 local storage에 첫 데이터 삽입 후 url list state 업데이트
+    */
+    localStorage.setData(
+      localStorageKeys.url_list,
+      JSON.stringify([
+        {
+          id: uniqueId,
+          url: urlSearchInput,
+        },
+      ])
+    );
+    setUrlSearchInput("");
+    return setUrlList((prevUrlList: Array<IUrlItem>) => [
+      ...prevUrlList,
+      {
+        id: uniqueId,
+        url: urlSearchInput,
+      },
+    ]);
+  };
+
+  const removeUrlHandler = (url_id: string) => {
+    setUrlList(urlList.filter((urlItem: IUrlItem) => urlItem.id !== url_id));
+
+    /* 만약 local storage에 저장된 url이 하나 라면, 전부 삭제 */
+    if (JSON.parse(urlFromLocalStorage).length === 1) {
+      return localStorage.removeData(localStorageKeys.url_list);
+    }
+
+    localStorage.setData(
+      localStorageKeys.url_list,
+      JSON.stringify(
+        JSON.parse(urlFromLocalStorage).filter(
+          (urlItem: IUrlItem) => urlItem.id !== url_id
+        )
+      )
+    );
+  };
+
+  useEffect(() => {
+    setUrlList(
+      JSON.parse(localStorage.getData(localStorageKeys.url_list) as string) ??
+        []
+    );
+  }, []);
+
+  return {
+    urlList,
+    urlSearchInput,
+    urlSearchInputHandler,
+    addUrlHandler,
+    removeUrlHandler,
+  };
+};
 
 export default useUrl;
--- src/pages/UrlList/UrlList.tsx
+++ src/pages/UrlList/UrlList.tsx
@@ -1,4 +1,3 @@
-import React, { useEffect, useState, useCallback } from "react";
 import Layout from "../../layouts/Layout";
 import {
   Input,
@@ -8,104 +7,67 @@ import {
   Card,
   IconButton,
   Button,
+  ButtonGroup,
+  Alert,
 } from "@material-tailwind/react";
-import { TrashIcon } from "@heroicons/react/24/solid";
+import { TrashIcon, InformationCircleIcon } from "@heroicons/react/24/solid";
 import { IUrlItem } from "../../@types/data.types";
-import localStorage from "../../utils/localStorage";
-import uuid from "react-uuid";
-import { localStorageKeys } from "../../constants/dummy";
+import useUrl from "../../hooks/useUrl";
 
 export default function UrlList() {
-  const [urlList, setUrlList] = useState<Array<IUrlItem>>([]);
-  const [urlSearchInput, setUrlSearchInput] = useState("");
-  const uniqueId = uuid();
-
-  const urlSearchInputHandler = (
-    event: React.ChangeEvent<HTMLInputElement>
-  ) => {
-    setUrlSearchInput(event.target.value);
-  };
-
-  const addUrlHandler = () => {
-    /* 중복체크, url 검증 */
-    const urlFromLocalStorage = localStorage.getData(
-      localStorageKeys.url_list
-    ) as string;
-    /*
-      local storage에 url_list라는 이름으로 url 데이터가 존재한다면
-      기존 데이터에 새로운 url 정보를 삽입 후 url list state 업데이트
-    */
-    if (urlFromLocalStorage) {
-      const addedUrlList = JSON.parse(urlFromLocalStorage).concat({
-        id: uniqueId,
-        url: urlSearchInput,
-      });
-      localStorage.setData(
-        localStorageKeys.url_list,
-        JSON.stringify(addedUrlList)
-      );
-      setUrlSearchInput("");
-      return setUrlList(addedUrlList);
-    }
-    /*
-      local storage에 url_list라는 이름으로 url 데이터가 존재 하지 않는다면
-      url_list라는 이름으로 local storage에 첫 데이터 삽입 후 url list state 업데이트
-    */
-    localStorage.setData(
-      localStorageKeys.url_list,
-      JSON.stringify([
-        {
-          id: uniqueId,
-          url: urlSearchInput,
-        },
-      ])
-    );
-    setUrlSearchInput("");
-    return setUrlList((prevUrlList: Array<IUrlItem>) => [
-      ...prevUrlList,
-      {
-        id: uniqueId,
-        url: urlSearchInput,
-      },
-    ]);
-  };
-
-  useEffect(() => {
-    setUrlList(
-      JSON.parse(localStorage.getData(localStorageKeys.url_list) as string) ??
-        []
-    );
-  }, []);
+  const {
+    urlList,
+    urlSearchInput,
+    urlSearchInputHandler,
+    addUrlHandler,
+    removeUrlHandler,
+  } = useUrl();
 
   return (
     <Layout>
       <div className="w-96">
-        <div className="flex">
+        <div className="flex mb-2">
           <Input
             label="Enter URL"
             value={urlSearchInput}
             onChange={urlSearchInputHandler}
           />
-          <Button className="ml-2" onClick={addUrlHandler}>
-            Button
-          </Button>
+          <ButtonGroup className="ml-2">
+            <Button onClick={addUrlHandler} ripple={false} variant="outlined">
+              <span>Add</span>
+            </Button>
+          </ButtonGroup>
         </div>
-        {!!urlList.length && (
-          <Card>
+        <Card className="h-[18rem]">
+          {urlList.length ? (
             <List>
               {urlList.map((list: IUrlItem) => (
-                <ListItem ripple={false} className="py-1 pr-1 pl-4">
+                <ListItem
+                  key={list.id}
+                  ripple={false}
+                  className="py-1 pr-1 pl-4"
+                >
                   {list.url}
                   <ListItemSuffix>
-                    <IconButton variant="text" color="blue-gray">
+                    <IconButton
+                      variant="text"
+                      color="blue-gray"
+                      onClick={() => {
+                        removeUrlHandler(list.id);
+                      }}
+                    >
                       <TrashIcon className="h-5 w-5" />
                     </IconButton>
                   </ListItemSuffix>
                 </ListItem>
               ))}
             </List>
-          </Card>
-        )}
+          ) : (
+            <div className="h-full w-full flex justify-center items-center">
+              Why don't you add your URLs?
+            </div>
+          )}
+        </Card>
       </div>
     </Layout>
   );
